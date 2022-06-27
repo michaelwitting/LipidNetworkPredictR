3faae90a68997a7a7d78ba314ffea577cfc47758
tnaake <thomasnaake@gmx.de>
2022-06-27 11:49:06 +0200

    create test_rhea, unit tests for getRheaIDsFromProteinID

---
+++ tests/testthat/test_rhea.R
@@ -0,0 +1,17 @@
+## function getRheaIDsFromProteinID
+
+test_that("getRheaIDsFromProteinID works.", {
+    
+    expect_equal(getRheaIDsFromProteinID("Q920L6"), 
+        c("RHEA:32727", "RHEA:32728", "RHEA:35315", "RHEA:35316", "RHEA:39675",
+            "RHEA:39676", "RHEA:60140", "RHEA:60141", "RHEA:39167", 
+            "RHEA:39168", "RHEA:36511", "RHEA:36512", "RHEA:36503",
+            "RHEA:36504", "RHEA:36523", "RHEA:36524"))
+    expect_equal(getRheaIDsFromProteinID("Q9H5J4"), 
+        c("RHEA:32727", "RHEA:35315", "RHEA:35316", "RHEA:39675", "RHEA:39676",
+            "RHEA:60140", "RHEA:60141", "RHEA:39167", "RHEA:39168",
+            "RHEA:36511", "RHEA:36512", "RHEA:36503", "RHEA:36504",
+            "RHEA:36523", "RHEA:36524"))
+    expect_error(getRheaIDsFromProteinID("Q92"), 
+        "Internet connection problem occured.")
+})
