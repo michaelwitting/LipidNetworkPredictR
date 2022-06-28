e2f508b0c2bb8c38c8f392efa3fd9b5510a4853c
tnaake <thomasnaake@gmx.de>
2022-06-28 12:14:51 +0200

    add minimal man page to r_GPStrGen

--- R/Utils_lipidmapstools.R
+++ R/Utils_lipidmapstools.R
@@ -1,10 +1,20 @@
+#' @name r_GPStrGen
+#' 
+#' @title r_GPStrGen
+#' 
+#' @param x \code{matrix} or \code{data.frame}
+#' @param file_name \code{character(1)}
+#'
+#' @author Michael Witting, \email{michael.witting@@helmholtz-muenchen.de}
+#' 
+#' @importFrom utils write.table
 r_GPStrGen <- function(x, file_name) {
   
   # get path of perl script
-  tools_path <-  system.file("extdata/lipidmapstools/bin", file = "GPStrGen.pl", package = "wormLipidPredictR")
+  tools_path <-  system.file("extdata/lipidmapstools/bin", file = "GPStrGen.pl", package = "LipidNetworkPredictR")
   
   # write clipboard
-  write.table(x, "clipboard.txt", col.names = FALSE, row.names = FALSE, quote = FALSE)
+  utils::write.table(x, "clipboard.txt", col.names = FALSE, row.names = FALSE, quote = FALSE)
 
   cmd <- paste0("perl ",
                 tools_path,
@@ -14,5 +24,4 @@ r_GPStrGen <- function(x, file_name) {
 
   # create command
   shell(cmd)
-
 }
