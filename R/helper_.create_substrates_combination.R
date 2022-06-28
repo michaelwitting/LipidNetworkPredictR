29faa4ec10d8b211a997977b899185245ddf51e2
tnaake <thomasnaake@gmx.de>
2022-06-28 12:14:07 +0200

    simplify internal variable naming and update man

--- R/helper_.create_substrates_combination.R
+++ R/helper_.create_substrates_combination.R
@@ -27,7 +27,7 @@
 #' substrates <- list(FA = FA)
 #' 
 #' ## create data.frame of substrates
-#' wormLipidPredictR:::.create_substrates_combinations(
+#' LipidNetworkPredictR:::.create_substrates_combinations(
 #'     substrates = substrates, 
 #'     constraints = "", negate = FALSE)
 .create_substrates_combinations <- function(substrates = substrates, 
@@ -88,10 +88,10 @@
 #' substrates <- list(FA = FA)
 #' 
 #' ## create data.frame of substrates
-#' df_substrates <- wormLipidPredictR:::.create_substrates_combinations(
+#' df_substrates <- LipidNetworkPredictR:::.create_substrates_combinations(
 #'     substrates = substrates, 
 #'     constraints = "", negate = FALSE)
-#' wormLipidPredictR:::.check_colnames_substrates_combinations(
+#' LipidNetworkPredictR:::.check_colnames_substrates_combinations(
 #'     df = df_substrates, reaction = "fa_to_coa")
 .check_colnames_substrates_combinations <- function(df, reaction = "fa_to_coa") {
     
@@ -99,228 +99,228 @@
         stop("'reaction' has to be of length 1")
     
     if (reaction == "acdhap_to_alkyldhap")
-        .cols <- c("ACDHAP", "FATOH")
+        cols <- c("ACDHAP", "FATOH")
     
     if (reaction == "alkyldhap_to_lpao")
-        .cols <- c("ALKYLDHAP")
+        cols <- c("ALKYLDHAP")
     
     if (reaction == "c1p_to_cer")
-        .cols <- c("C1P")
+        cols <- c("C1P")
     
     if (reaction == "cdpdg_to_pgp")
-        .cols <- c("CDPDG")
+        cols <- c("CDPDG")
     
     if (reaction == "cdpdg_to_pi")
-        .cols <- c("CDPDG")
+        cols <- c("CDPDG")
     
     if (reaction == "cer_to_c1p")
-        .cols <- c("CER")
+        cols <- c("CER")
     
     if (reaction == "cer_to_glccer")
-        .cols <- c("CER")
+        cols <- c("CER")
     
     if (reaction == "cer_to_sm")
-        .cols <- c("CER")
+        cols <- c("CER")
     
     if (reaction == "coa_to_acdhap")
-        .cols <- c("CoA")
+        cols <- c("CoA")
     
     if (reaction == "coa_to_fatoh")
-        .cols <- c("CoA")
+        cols <- c("CoA")
     
     if (reaction == "coa_to_lpa")
-        .cols <- c("CoA")
+        cols <- c("CoA")
     
     if (reaction == "dg_to_sn1mg")
-        .cols <- c("DG")
+        cols <- c("DG")
 
     if (reaction == "dg_to_sn2mg")
-        .cols <- c("DG")
+        cols <- c("DG")
     
     if (reaction == "dg_to_pa")
-        .cols <- c("DG")
+        cols <- c("DG")
     
     if (reaction == "dg_to_pc")
-        .cols <- c("DG")
+        cols <- c("DG")
     
     if (reaction == "dg_to_pe")
-        .cols <- c("DG")
+        cols <- c("DG")
     
     if (reaction == "dg_to_tg")
-        .cols <- c("DG", "CoA")
+        cols <- c("DG", "CoA")
     
     if (reaction == "dgo_to_pco")
-        .cols <- c("DGO")
+        cols <- c("DGO")
     
     if (reaction == "dgo_to_peo")
-        .cols <- c("DGO")
+        cols <- c("DGO")
     
     if (reaction == "dhcer_to_cer")
-        .cols <- c("DHCER")
+        cols <- c("DHCER")
     
     if (reaction == "dhcer_to_dhsm")
-        .cols <- c("DHCER")
+        cols <- c("DHCER")
     
     if (reaction == "dhsm_to_dhcer")
-        .cols <- c("DHSM")
+        cols <- c("DHSM")
     
     if (reaction == "fa_to_coa")
-        .cols <- c("FA")
+        cols <- c("FA")
     
     if (reaction == "lnape_to_gpnae")
-        .cols <- c("LNAPE")
+        cols <- c("LNAPE")
     
     if (reaction == "lpa_to_pa")
-        .cols <- c("LPA", "CoA")
+        cols <- c("LPA", "CoA")
     
     if (reaction == "lpao_to_pao")
-        .cols <- c("LPAO", "CoA")
+        cols <- c("LPAO", "CoA")
     
     if (reaction == "sn1lpc_to_fa")
-        .cols <- c("sn1LPC")
+        cols <- c("sn1LPC")
     
     if (reaction == "sn2lpc_to_fa")
-        .cols <- c("sn2LPC")
+        cols <- c("sn2LPC")
     
     if (reaction == "sn1lpc_to_pc")
-        .cols <- c("sn1LPC", "CoA")
+        cols <- c("sn1LPC", "CoA")
     
     if (reaction == "sn1lpe_to_fa")
-        .cols <- c("sn1LPE")
+        cols <- c("sn1LPE")
     
     if (reaction == "sn2lpe_to_fa")
-        .cols <- c("sn2LPE")
+        cols <- c("sn2LPE")
     
     if (reaction == "sn1lpe_to_pe")
-        .cols <- c("sn1LPE", "CoA")
+        cols <- c("sn1LPE", "CoA")
     
     if (reaction == "lpeo_to_peo")
-        .cols <- c("LPEO", "CoA")
+        cols <- c("LPEO", "CoA")
     
     if (reaction == "lpep_to_pep")
-        .cols <- c("LPEP", "CoA")
+        cols <- c("LPEP", "CoA")
     
     if (reaction == "sn1mg_to_dg")
-        .cols <- c("sn1MG", "CoA")
+        cols <- c("sn1MG", "CoA")
     
     if (reaction == "sn2mg_to_dg")
-        .cols <- c("sn2MG", "CoA")
+        cols <- c("sn2MG", "CoA")
     
     if (reaction == "sn1mg_to_fa")
-        .cols <- c("sn1MG")
+        cols <- c("sn1MG")
     
     if (reaction == "sn2mg_to_fa")
-        .cols <- c("sn2MG") 
+        cols <- c("sn2MG") 
     
     if (reaction == "sn1mg_to_lpa")
-        .cols <- c("sn1MG")
+        cols <- c("sn1MG")
     
     if (reaction == "sn2mg_to_sn1mg")
-        .cols <- c("sn2MG")
+        cols <- c("sn2MG")
     
     if (reaction == "nae_to_fa")
-        .cols <- c("NAE")
+        cols <- c("NAE")
     
     if (reaction == "nape_to_lnape")
-        .cols <- c("NAPE")
+        cols <- c("NAPE")
     
     if (reaction == "nape_to_nae")
-        .cols <- c("NAPE")
+        cols <- c("NAPE")
     
     if (reaction == "nape_to_pnae")
-        .cols <- c("NAPE")
+        cols <- c("NAPE")
     
     if (reaction == "napeo_to_nae")
-        .cols <- c("NAPEO")
+        cols <- c("NAPEO")
     
     if (reaction == "pa_to_cdpdg")
-        .cols <- c("PA")
+        cols <- c("PA")
     
     if (reaction == "pa_to_dg")
-        .cols <- c("PA")
+        cols <- c("PA")
     
     if (reaction == "pao_to_dgo")
-        .cols <- c("PAO")
+        cols <- c("PAO")
     
     if (reaction == "pc_to_dg")
-        .cols <- c("PC")
+        cols <- c("PC")
     
     if (reaction == "pc_to_sn1lpc")
-        .cols <- c("PC")
+        cols <- c("PC")
     
     if (reaction == "pc_to_sn2lpc")
-        .cols <- c("PC")
+        cols <- c("PC")
     
     if (reaction == "pc_to_pa")
-        .cols <- c("PC")
+        cols <- c("PC")
     
     if (reaction == "pc_to_ps")
-        .cols <- c("PC")
+        cols <- c("PC")
     
     if (reaction == "pco_to_lpco")
-        .cols <- c("PCO")
+        cols <- c("PCO")
     
     if (reaction == "pe_to_dg")
-        .cols <- c("PE")
+        cols <- c("PE")
     
     if (reaction == "pe_to_sn1lpe")
-        .cols <- c("PE")
+        cols <- c("PE")
     
     if (reaction == "pe_to_sn2lpe")
-        .cols <- c("PE")
+        cols <- c("PE")
     
     if (reaction == "pe_to_nape_sn1")
-        .cols <- c("PE", "PC")
+        cols <- c("PE", "PC")
     
     if (reaction == "pe_to_nape_sn2")
-        .cols <- c("PE", "PC")
+        cols <- c("PE", "PC")
     
     if (reaction == "pe_to_pa")
-        .cols <- c("PE")
+        cols <- c("PE")
     
     if (reaction == "pe_to_ps")
-        .cols <- c("PE")
+        cols <- c("PE")
     
     if (reaction == "peo_to_lpeo")
-        .cols <- c("PEO")
+        cols <- c("PEO")
     
     if (reaction == "peo_to_napeo_sn1")
-        .cols <- c("PEO", "PC")
+        cols <- c("PEO", "PC")
     
     if (reaction == "peo_to_napeo_sn2")
-        .cols <- c("PEO", "PC")
+        cols <- c("PEO", "PC")
     
     if (reaction == "peo_to_pep")
-        .cols <- c("PEO")
+        cols <- c("PEO")
     
     if (reaction == "pep_to_lpep")
-        .cols <- c("PEP")
+        cols <- c("PEP")
     
     if (reaction == "pep_to_napep_sn1")
-        .cols <- c("PEP", "PC")
+        cols <- c("PEP", "PC")
     
     if (reaction == "pep_to_napep_sn2")
-        .cols <- c("PEP", "PC")
+        cols <- c("PEP", "PC")
     
     if (reaction == "pg_to_cl")
-        .cols <- c("PG", "CDPDG")
+        cols <- c("PG", "CDPDG")
     
     if (reaction == "pgp_to_pg")
-        .cols <- c("PGP")
+        cols <- c("PGP")
     
     if (reaction == "ps_to_pe")
-        .cols <- c("PS")
+        cols <- c("PS")
     
     if (reaction == "sm_to_cer")
-        .cols <- c("SM")
+        cols <- c("SM")
     
     if (reaction == "sphinga_to_dhcer")
-        .cols <- c("CoA")
+        cols <- c("CoA")
     
     if (reaction == "tg_to_dg")
-        .cols <- c("TG")
+        cols <- c("TG")
     
-    if (!all(.cols %in% colnames(df)))
-        stop(paste("columns", paste(sprintf("%s", .cols), collapse = ", "), 
+    if (!all(cols %in% colnames(df)))
+        stop(paste("columns", paste(sprintf("%s", cols), collapse = ", "), 
             "not in 'colnames(df)'"))
 }
