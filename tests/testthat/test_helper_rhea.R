d81a18330af4c52ed0e9fc6b3e492ad648435d33
Thomas Naake <thomasnaake@gmx.de>
2024-09-30 16:18:13 +0200

    Update test_helper_rhea.R

--- tests/testthat/test_helper_rhea.R
+++ tests/testthat/test_helper_rhea.R
@@ -1,10 +1,10 @@
 ## function .getRheaIDsFromProteinID
 test_that(".getRheaIDsFromProteinID works", {
-    rhea <- tryCatch(getRheaIDsFromProteinID("Q920L6"), error = function(e) NULL)
-    if (!is.null(rhea))
-        expect_equal(rhea,
+    if (Sys.info()["sysname"] != "Darwin") {
+        expect_equal(getRheaIDsFromProteinID("Q920L6"),
             c("RHEA:32727", "RHEA:35315", "RHEA:39675", "RHEA:60140", "RHEA:39167",
                 "RHEA:36511", "RHEA:36503", "RHEA:36523"))
+    } 
     expect_error(getRheaIDsFromProteinID(NULL),
         "Failed to retrieve data from UniProt. Check the UniProt ID and try again.")
     expect_error(getRheaIDsFromProteinID("foo"),
